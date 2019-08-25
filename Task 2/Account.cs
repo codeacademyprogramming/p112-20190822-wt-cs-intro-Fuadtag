14113eb4c3d7e0d1ecbdda464fa95b73247c3e1b
Fuadtag <Fuadjt@code.edu.az>
2019-08-25 19:48:05 +0400

    Task2 constructor created

---
+++ Task 2/Account.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Class1
+{
+	public Account()
+	{
+        string Name;
+        string Surname;
+        string Id;
+	}
+    public Account(string Name, string Surname, string Id)
+    {
+        Name = name;
+        Surname = surname;
+        Id = id;
+
+    }
+}
---
+++ Task 2/program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Class1
+{
+    class Person
+    {
+        
+        string name = Console.ReadLine();
+        string surname = Console.ReadLine();
+        string id = Console.ReadLine();
+        
+    }
+}
